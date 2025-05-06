ac5f5de8ecb674917e8262dcb659fc1f67defb36
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-05-06 19:59:38 +0530

    Files

--- rest_api/index.js
+++ rest_api/index.js
@@ -5,6 +5,15 @@ const port = 8080;
 
 const path = require("path");
 
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req,res)=>{
+    res.send("Server working well");
+});
 
 app.listen(port, ()=>{
     console.log(`App is listening on port ${port}`);
