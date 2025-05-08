38cac7f7eee6bbdcdf7f8e15acf10c6858dcf603
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-05-08 19:08:05 +0530

    files

--- rest_api/index.js
+++ rest_api/index.js
@@ -13,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id: "1a",
         username: "Thomas Shelby",
         content: "I love to work"
     },
     {
+        id: "2b",
         username: "Arthur Shelby",
         content: "I love swimming"
     },
     {
+        id: "3c",
         username: "John Shelby",
         content: "I love to play golf"
     },
@@ -45,6 +48,15 @@ app.post("/posts", (req,res)=>{
     posts.push({username, content}); //posts being the array we used earlier.
     // res.send("Post request received");
     res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req,res)=>{
+    let {id} = req.params; //id retrieve
+    // console.log(id);
+    let post = posts.find((p) => id === p.id); //find the post with the id where p = posts
+    // console.log(post);
+    // res.send("request working");
+    res.render("show.ejs", {post});
 });
 
 app.listen(port, ()=>{
